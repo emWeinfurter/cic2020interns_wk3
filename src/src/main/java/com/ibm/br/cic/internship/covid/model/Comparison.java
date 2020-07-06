4c8b2a82b2cb33218336236a20c791fb4e00d10d
Elise Weinfurter <elise.weinfurter@ibm.com>
2020-07-06 11:40:50 -0500

    Compare get and set

--- src/src/main/java/com/ibm/br/cic/internship/covid/model/Comparison.java
+++ src/src/main/java/com/ibm/br/cic/internship/covid/model/Comparison.java
@@ -18,13 +18,13 @@ public @Data class Comparison {
     @JsonProperty("Countries")
     private List<Country> countries;
 
-    public List<Country> getCountries() {
+    /*public List<Country> getCountries() {
 		return countries;
 	}
 
 	public void setCountries(List<Country> countries) {
 		this.countries = countries;
-	}
+	}*/
 	//Elise Weinfurter changes end
 
 	private CompareBy compareBy;
